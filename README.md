66f380846546457d6f7069524ca7b6047244343b
youpeterabb1t <21300259@handong.edu>
2020-05-23 00:34:28 +0900

    initial parsing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dong-uibogam
 dong-uibogam crawling
+
+Selenium 
+전남도립도서관 -> krpia -> 동의치료경험집성
+
+미완성
+-> 데이터 크롤링 및 파일 저장
---
+++ dong-uibogam.py
@@ -0,0 +1,54 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.alert import Alert
+
+from urllib import parse
+
+from instance import *
+
+options = Options()
+options.headless = False
+browser = webdriver.Chrome(executable_path="./chromedriver.exe", options=options)
+browser.get("https://lib.jeonnam.go.kr/bbs/login.php")
+
+browser.find_element_by_name('mb_id').send_keys(아이디)
+browser.find_element_by_name('mb_password').send_keys(비밀번호)
+browser.find_element_by_css_selector(".login form button").click()
+browser.get("https://lib.jeonnam.go.kr/bbs/board.php?bo_table=subscription_service#")
+browser.execute_script("krpia_open(3516)")
+browser.close()
+browser.switch_to.window(driver.window_handles[-1])
+browser.get("https://www.krpia.co.kr/product/main?plctId=PLCT00004558#none")
+browser.get("https://www.krpia.co.kr/viewer?plctId=PLCT00004558&tabNodeId=NODE03923082")
+
+#browser.implicitly_wait(3)
+
+
+
+# 게시판 글 읽기
+#browser.get(f"https://hisnet.handong.edu/cis/list.php?Board=KYOM_EXTRA&CateCode=2959")
+
+#browser.get(f"https://hisnet.handong.edu/cis/write.php?Board=KYOM_EXTRA&CateCode=2959&dflag=")
+#browser.find_element_by_name('subject').send_keys('loveetls')
+
+#WebElement formElement = driver.findElement(By.name("form_w"));
+#List<WebElement> allFormChildElements = formElement.findElements(By.xpath("*"));
+#browser.find_element_by_class_name('content')
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+#element = browser.find_element_by_name('content')
+#WebDriverWait(browser, 5).until(
+#    EC.presence_of_element_located((By.ID, "content"))
+#)  # Wait until the `text_to_score` element appear (up to 5 seconds)
+#browser.execute_script('oEditors.getById["content"].setIR("11")')
+#browser.find_element_by_name('form_w').submit()
+#browser.implicitly_wait(3)
+#browser.switch_to_frame('MyIFrame').switch_to_alert().accept();
+
